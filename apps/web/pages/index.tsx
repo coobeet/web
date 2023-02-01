5d89acdd065d88aa57c88ce43c75b607cc521b9d
coobeet <coobeet@gmail.com>
2023-02-01 21:31:44 +0800

    refactor: rename ui to @coobeet/ui

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,7 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { echo } from "@buf/coobeet_coobeet.bufbuild_connect-query/coobeet/v1/echo-EchoService_connectquery";
-import { useState } from "react";
+import { useMutation } from '@tanstack/react-query';
+import { echo } from '@buf/coobeet_coobeet.bufbuild_connect-query/coobeet/v1/echo-EchoService_connectquery';
+import { useState } from 'react';
+import { Button } from '@coobeet/ui';
 
 export default function Web() {
   const [input, setInput] = useState(``);
@@ -10,6 +11,7 @@ export default function Web() {
     <div>
       <h1>Web</h1>
       <p>
+        <Button />
         <button onClick={() => mutation.mutate({ message: input })}>
           Send
         </button>
